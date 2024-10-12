8c7c78b618076bba30b5d13fa08967319cc5d06d
shreygarg <crazymeshrey@gmail.com>
2024-10-12 15:52:33 +0530

    build working and server uRl updated

--- constants/Environment.js
+++ constants/Environment.js
@@ -1,1 +1,4 @@
-export const SERVERURL = "http://192.168.1.4:3001"
+// export const SERVERURL = "http://192.168.1.4:3001"
+
+export const SERVERURL = "https://vyayam.onrender.com"
+// https://vyayam.onrender.com
