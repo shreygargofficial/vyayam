5de67a43d86d7b8158b5ae6ea86e84ececcf3295
shreygarg <crazymeshrey@gmail.com>
2024-10-13 00:08:01 +0530

    URL server to railway

--- constants/Environment.js
+++ constants/Environment.js
@@ -1,4 +1,5 @@
 // export const SERVERURL = "http://192.168.1.4:3001"
 
-export const SERVERURL = "https://vyayam.onrender.com"
+export const SERVERURL = "https://healthometer-production.up.railway.app"
+//never give / after URL
 // https://vyayam.onrender.com
