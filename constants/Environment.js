d88ff3f6a7f51d52a58716b4ba5d0db50ab4d310
shreygarg <crazymeshrey@gmail.com>
2024-11-14 02:39:46 +0530

    typo in server url

--- constants/Environment.js
+++ constants/Environment.js
@@ -1,5 +1,5 @@
 // export const SERVERURL = "http://192.168.1.9:3001"
 
-export const SERVERURL = "https://c99-healthometer.up.railway.app/"
+export const SERVERURL = "https://c99-healthometer.up.railway.app"
 //never give / after URL
 // https://vyayam.onrender.com
