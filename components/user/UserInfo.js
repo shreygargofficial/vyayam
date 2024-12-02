2c57643d832b005d64bba502376f4dda73e07b01
shreygarg <crazymeshrey@gmail.com>
2024-12-02 16:28:39 +0530

    [HEALT-102]-user bmi tdee
    1. BMI done

--- components/user/UserInfo.js
+++ components/user/UserInfo.js
@@ -4,12 +4,20 @@ import ButtonWithBorder from "../ui/ButtonWithBorder";
 import { useDispatch } from "react-redux";
 import { logoutActionCreator } from "../../redux/ActionCreators/userActionsCreator";
 
-function UserInfo({ defaultValueUser: { firstName, lastName, gender, bio, targetedWeight, birthDate, height } }) {
+function UserInfo({ defaultValueUser: { firstName, lastName, gender, bio, targetedWeight, birthDate, height, weight } }) {
     let dispatch = useDispatch()
+    const BMI = ((weight * 10000) / (height * height)).toFixed(2)
 
     const logoutHandler = () => {
         dispatch(logoutActionCreator())
     }
+    let classBMI =
+        BMI < 18.5 ? 'underweight' :
+            (BMI >= 18.5 && BMI <= 24.9) ? 'normal' :
+                (BMI >= 25 && BMI <= 29.9) ? 'overweight' :
+                    (BMI >= 30 && BMI <= 34.9) ? 'obese class 1 (moderate)' :
+                        (BMI >= 35 && BMI <= 39.9) ? 'obese class 2 (severe)' :
+                            'obese class 3 (very severe)';
 
     return (
 
@@ -20,6 +28,8 @@ function UserInfo({ defaultValueUser: { firstName, lastName, gender, bio, target
                 <Text style={styles.text}>Gener : {gender}</Text>
                 <Text style={styles.text}>Bio : {bio}</Text>
                 <Text style={styles.text}>Height: {height}cm</Text>
+                <Text style={styles.text}>Weight: {weight}kg</Text>
+                <Text style={[styles.text]}>BMI: <Text style={styles[classBMI]}>{BMI} </Text><Text style={[styles[classBMI], classBMI]}>({classBMI})</Text></Text>
                 <Text style={styles.text}>Targeted Weight : {targetedWeight}kg</Text>
                 <Text style={styles.text}>Age : {new Date().getFullYear() - birthDate}</Text>
             </View>
@@ -36,7 +46,6 @@ export default UserInfo;
 
 const styles = StyleSheet.create({
     root: {
-
     },
 
     infoContainer: {
@@ -61,5 +70,26 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 20
         // width: '50%'
-    }
+    },
+    normal: {
+        color: colors.green
+    },
+    underweight: {
+        color: colors.red
+    },
+    overweight: {
+        color: colors.red
+    },
+    classBMI: {
+        fontSize: 10,
+    },
+    'obese class 1 (moderate)': {
+        color: colors.red
+    },
+    'obese class 2 (severe)': {
+        color: colors.red
+    },
+    'obese class 3 (very severe)': {
+        color: colors.red
+    },
 })
