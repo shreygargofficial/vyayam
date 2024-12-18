22419bea7967f4ebe3f1be21a954da0977687dd2
shreygarg <crazymeshrey@gmail.com>
2024-12-19 00:15:52 +0530

    environment url reversed

--- constants/Environment.js
+++ constants/Environment.js
@@ -1,4 +1,4 @@
-export const SERVERURL = "http://192.168.1.4:3001"
+// export const SERVERURL = "http://192.168.1.4:3001"
 
-// export const SERVERURL = "https://c99-healthometer.up.railway.app"
+export const SERVERURL = "https://c99-healthometer.up.railway.app"
 //never give / after URL
