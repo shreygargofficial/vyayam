f5fdd589df0eafc1c8e863a34660f5f035148576
shreygarg <crazymeshrey@gmail.com>
2024-12-19 01:48:24 +0530

    [HEALT-122]-BMI and Diet veg filter and sort
    [HEALT-122](https://app.plane.so/healthometer/projects/3376a6bb-4da6-45f8-8c9f-ea28fe279677/issues/e56bfc62-2a9f-49a5-a168-83327f5b38b4)

--- components/screen/nativeStack/weightManagement/AllMealsForWeightManagement.js
+++ components/screen/nativeStack/weightManagement/AllMealsForWeightManagement.js
@@ -1,6 +1,6 @@
 import { useRoute } from "@react-navigation/native";
-import { useEffect, useMemo } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Switch, Text, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import DietCard from "../../../weightGainLoss/DietCard";
 import { allDietsFetchActionCreator } from "../../../../redux/ActionCreators/dietActionCreator";
@@ -11,12 +11,33 @@ function AllMealsForWeightManagement() {
     const type = params?.type;
     const tdee = params?.tdee
     const resultCalories = type === "loss" ? tdee - 500 : tdee + 500;
-    const diet = useSelector(state => state.diet)
+    const diet = useSelector(state => state.diet);
+    const [dietData, setDietData] = useState([])
+    const [vegEnabled, setVegEnabled] = useState(false)
+
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(allDietsFetchActionCreator())
     }, []);
 
+    useEffect(() => {
+        if (diet.dietData?.length > 0) {
+            let unsortedDietData = []
+            if (vegEnabled) {
+                unsortedDietData = diet.dietData.filter(ele => ele.veg == true)
+            }
+            else {
+                unsortedDietData = [...diet.dietData]
+            }
+            let sortedDiet = unsortedDietData.sort((a, b) => a.mealTotal - b.mealTotal);
+            setDietData(sortedDiet);
+        }
+    }, [diet.dietData, vegEnabled]);
+
+
+    const toggleVegSwitch = () => {
+        setVegEnabled(prev => !prev)
+    }
     return (
         <View style={styles.root}>
             <ScrollView
@@ -29,8 +50,21 @@ function AllMealsForWeightManagement() {
                         You need to eat around {resultCalories} kcal for weight {type}. There are diets mentioned below that can fulfill your desired daily calories intake. Click on each to get details
                     </Text>
                 </View>}
-                {diet?.dietData && diet.dietData?.length &&
-                    diet?.dietData.map((ele, index) => {
+                <View style={styles.switchContainer}>
+
+                    <Switch
+                        trackColor={{ false: colors.grey, true: colors.green }}
+                        ios_backgroundColor="#3e3e3e"
+                        style={styles.switch}
+                        thumbColor={colors.white}
+                        onValueChange={toggleVegSwitch}
+                        value={vegEnabled}
+                    />
+                    <Text style={styles.switchText}>Veg</Text>
+                </View>
+
+                {dietData && dietData?.length > 0 &&
+                    dietData.map((ele, index) => {
                         return (
                             <DietCard key={ele._id} item={ele} index={index + 1} />
                         )
@@ -48,9 +82,31 @@ const styles = StyleSheet.create({
         backgroundColor: colors.white,
         flex: 1
     },
+    rootCenter: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     mainWrapper: {
         paddingHorizontal: 30,
         marginVertical: 30
+    },
+    switchContainer: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        justifyContent: 'flex-end',
+        width: '100%',
+        alignItems: 'center',
+
+    },
+    switchText: {
+        paddingHorizontal: 10,
+        fontWeight: '500',
+        letterSpacing: 1,
+
+    },
+    switch: {
+        transform: [{ scaleX: 0.8 }, { scaleY: 0.8 }],
     },
     nutritionContent: {
         marginVertical: 10,
